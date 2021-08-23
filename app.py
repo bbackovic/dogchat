f6ce49b7d413861459de5520d060d16062b485a0
my name <myemailid@domain.com>
2021-08-22 20:42:57 -0400

    improved post data model

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def feed():
 
-    test_posts = [
-        "I can't go to the park today",
-        "I could really use a treat right now",
-        "Aren't naps the best?"
-    ]
+    post1 = {
+        "Text": "I can't wait to go to the park today!",
+        "Name": "Cloud", 
+        "Username": "cloud",
+        "LikeCount": 10, 
+        "CommentCount": 4,
+        "DateTime": datetime(2021, 7, 1, 17, 0, 0),
+        "Picture": 'Dog1.jpg'
+    }
+    post2 = {
+        "Text": "Me too! Can I join in?",
+        "Name": "Fluffy", 
+        "Username": "fluffy",
+        "LikeCount": 5, 
+        "CommentCount": 4,
+        "DateTime": datetime(2021, 7, 1, 19, 0, 0),
+        "Picture": 'doggy.jpg'
+    }
+    post3 = {
+        "Text": "Yes! Ruff ruff",
+        "Name": "Cloud", 
+        "Username": "cloud",
+        "LikeCount": 8, 
+        "CommentCount": 2,
+        "DateTime": datetime(2021, 7, 1, 20, 30, 0),
+        "Picture": 'Dog1.jpg'
+    }
     
+    test_posts = [post1, post2, post3]
+
     return render_template('index.html', posts=test_posts)
